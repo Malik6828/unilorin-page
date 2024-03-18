547ac8cf5a1dbdbbe9990d5a58009d45f38fca32
OLAK <abdulmalikajadi@gmail.com>
2024-03-18 04:48:10 -0700

    second-commit

---
+++ myscript.js
@@ -0,0 +1,153 @@
+var close = document.querySelector('.side-list');
+var close2 = document.querySelector('.side-list2');
+var close3 = document.querySelector('.side-list3');
+var close4 = document.querySelector('.side-list4');
+var content =document.querySelector('.div-side');
+var content2 =document.querySelector('.div-side1');
+var content3 =document.querySelector('.div-side3');
+var content4 =document.querySelector('.div-side4');
+// var content = document.querySelector('#sub-sidebar');
+// var content2 =document.querySelector('#sub-sidebar1');
+// var content3 =document.querySelector('#sub-sidebar2');
+
+close.addEventListener('click', function(){
+   
+    
+    content.style.display='block';
+    content2.style.display='none';
+    content3.style.display='none';
+    content4.style.display='none';
+    close.style.backgroundColor='rgba(255, 255, 0, 0.7)';
+    close2.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close3.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close4.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content2.style.display='block';
+    // content3.style.display='block';
+    
+})
+close.addEventListener('dblclick', function(){
+   
+    
+    content.style.display='none';
+    close.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close2.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    
+    // content2.style.display='none';
+    // content3.style.display='none';
+    
+})
+
+var close2 = document.querySelector('.side-list2');
+var close = document.querySelector('.side-list');
+var close3 = document.querySelector('.side-list3');
+var close4 = document.querySelector('.side-list4');
+var content2 =document.querySelector('.div-side1');
+var content =document.querySelector('.div-side');
+var content3 =document.querySelector('.div-side3');
+var content4 =document.querySelector('.div-side4');
+var close = document.querySelector('.side-list');
+// var content4 = document.querySelector('#sub-sidebar3');
+// var content5 =document.querySelector('#sub-sidebar4');
+// var content6 =document.querySelector('#sub-sidebar5');
+
+close2.addEventListener('click', function(){
+   
+    
+    content2.style.display='block';
+    content.style.display='none';
+    content3.style.display='none';
+    content4.style.display='none';
+    close2.style.backgroundColor='rgba(255, 255, 0, 0.7)';
+    close.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close3.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close4.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content5.style.display='block';
+    // content6.style.display='block';
+    
+})
+close2.addEventListener('dblclick', function(){
+   
+    
+    content2.style.display='none';
+    close2.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content5.style.display='none';
+    // content6.style.display='none';
+    
+})
+
+var close3 = document.querySelector('.side-list3');
+var close4 = document.querySelector('.side-list4');
+var close2 = document.querySelector('.side-list2');
+var close = document.querySelector('.side-list');
+var content =document.querySelector('.div-side');
+var content2 =document.querySelector('.div-side1');
+var content3 =document.querySelector('.div-side3');
+var content4 =document.querySelector('.div-side4');
+// var content7 = document.querySelector('#sub-sidebar6');
+// var content8 =document.querySelector('#sub-sidebar7');
+// var content9 =document.querySelector('#sub-sidebar8');
+
+close3.addEventListener('click', function(){
+   
+    
+    content3.style.display='block';
+    content4.style.display='none';
+    content.style.display='none';
+    content2.style.display='none';
+    close3.style.backgroundColor='rgba(255, 255, 0, 0.7)';
+    close4.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close2.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content8.style.display='block';
+    // content9.style.display='block';
+    
+})
+close3.addEventListener('dblclick', function(){
+   
+    
+    content3.style.display='none';
+    close3.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close4.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content8.style.display='none';
+    // content9.style.display='none';
+    
+})
+var close4 = document.querySelector('.side-list4');
+var close3 = document.querySelector('.side-list3');
+var close = document.querySelector('.side-list');
+var close2 = document.querySelector('.side-list2');
+var content =document.querySelector('.div-side');
+var content2 =document.querySelector('.div-side1');
+var content3 =document.querySelector('.div-side3');
+var content4 =document.querySelector('.div-side4');
+// var content10 = document.querySelector('#sub-sidebar9');
+// var content11 =document.querySelector('#sub-sidebar10');
+// var content12 =document.querySelector('#sub-sidebar11');
+
+close4.addEventListener('click', function(){
+   
+    
+    content4.style.display='block';
+    content3.style.display='none';
+    content.style.display='none';
+    content2.style.display='none';
+    close4.style.backgroundColor='rgba(255, 255, 0, 0.7)';
+    close3.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close2.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content11.style.display='block';
+    // content12.style.display='block';
+    
+})
+close4.addEventListener('dblclick', function(){
+   
+    
+    content4.style.display='none';
+    close4.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    close3.style.backgroundColor='rgba(179, 179, 233, 0.3)';
+    // content11.style.display='none';
+    // content12.style.display='none';
+    
+})
+
